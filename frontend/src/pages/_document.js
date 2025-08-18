3b1c56fbb27cd2eb715b5df0368698df2eff9fae
laxmankori03 <laxmankori369@gmail.com>
2025-08-18 10:02:26 +0530

    Update _document.js

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
       <body className="antialiased" style={{backgroundColor:"#1E1E2F",color:"#F5F5F5"}}>
         <Main />
         <NextScript />
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" />
       </body>
     </Html>
   );
